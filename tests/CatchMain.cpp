4111ea757cf6684112b76c62894b2264499eb510
Arjun Variar <accio.arjun@gmail.com>
2020-09-26 22:05:13 +0530

    Better Naming.

--- tests/CatchMain.cpp
+++ tests/CatchMain.cpp
@@ -15,7 +15,7 @@ TEST_CASE("Testing English", "[english]") {
     auto time = (float) ((end - start) / (CLOCKS_PER_SEC / 1000));
     INFO(XL("Library loaded: ") << time << XL(" ms"));
 
-    SECTION("WORD SEGMENTATION") {
+    SECTION("Do Word Segmentation") {
         std::unordered_map<std::string, std::string> sentences = {
                 {"thequickbrownfoxjumpsoverthelazydog",                                                "they quick brown fox jumps over therapy dog"},
                 {"itwasabrightcolddayinaprilandtheclockswerestrikingthirteen",                         "it was bright holiday in april another clocks were striking thirteen"},
@@ -28,7 +28,7 @@ TEST_CASE("Testing English", "[english]") {
         }
     }
 
-    SECTION("SPELL CORRECTION") {
+    SECTION("Do Spell Correction") {
         std::unordered_map<std::string, std::string> words = {
                 {"tke",          "take"},
                 {"abolution",    "abolition"},
@@ -41,7 +41,7 @@ TEST_CASE("Testing English", "[english]") {
         }
     }
 
-    SECTION("COMPOUND MISTAKES") {
+    SECTION("Correct Compound Mistakes") {
         std::unordered_map<std::string, std::string> compunded_sentences = {
                 {"whereis th elove hehad dated forImuch of thepast who couqdn'tread in sixthgrade and ins pired him", "whereas to love head dated for much of theist who couldn't read in sixth grade and inspired him"},
                 {"in te dhird qarter oflast jear he hadlearned ofca sekretplan",                                      "in to third quarter of last year he had learned of a secret plan"},
