3968572cde37bd65d32ce05390c9d768aeee5195
Arjun Variar <accio.arjun@gmail.com>
2020-09-28 18:42:59 +0530

    Changes as per https://github.com/pybind/pybind11/issues/1430\#issuecomment-484520887

--- setup.py
+++ setup.py
@@ -51,6 +51,10 @@ class CMakeBuild(build_ext):
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
 
+        # Pile all .so in one place and use $ORIGIN as RPATH
+        cmake_args += ["-DCMAKE_BUILD_WITH_INSTALL_RPATH=TRUE"]
+        cmake_args += ["-DCMAKE_INSTALL_RPATH={}".format("$ORIGIN")]
+
         if platform.system() == "Windows":
             cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
             if sys.maxsize > 2 ** 32:
@@ -66,7 +70,7 @@ class CMakeBuild(build_ext):
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
         subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+        subprocess.check_call(['cmake', '--build', '.', '--target', os.path.basename(ext.name)] + build_args, cwd=self.build_temp)
 
 
 setup(
@@ -80,4 +84,8 @@ setup(
     ext_modules=[CMakeExtension('SymSpellCppPy')],
     cmdclass=dict(build_ext=CMakeBuild),
     zip_safe=False,
+    url="https://github.com/viig99/SymSpellCppPy",
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+    ]
 )
